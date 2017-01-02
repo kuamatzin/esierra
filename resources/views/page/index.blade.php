afd49bf9a59116ddcc21d5d15feb560d1d8bf6e4
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-01-02 13:00:33 -0600

    Mapa actualizado

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -266,7 +266,7 @@
                         <div class="col-md-12">
                             <h2 class="mg-sec-left-title">¿Dónde estamos?</h2>
                             <br>
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15092.626595817006!2d-98.1794781!3d18.968688!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdadde0b99c1be5f8!2sHerramientas+Cela!5e0!3m2!1ses-419!2smx!4v1479425045172" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3752.5832153082006!2d-97.58818378508657!3d19.857580486644338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85da973795dc09bb%3A0x4ccdda7e28328812!2sCaba%C3%B1as+Entrada+a+la+Sierra!5e0!3m2!1ses-419!2smx!4v1483383586669" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
                         </div>
                     </div>
                 </div>
