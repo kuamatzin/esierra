1baaa8f4490c11c8d0106095d90897470568e40a
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-02-06 10:12:14 -0600

    Registro

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $cabañas = Cabana::orderBy('tipo')->get();
--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
+    public function __construct()
+    {
+        dd("HOLA");
+    }
+
     public function index()
     {
         return view('page.index');
