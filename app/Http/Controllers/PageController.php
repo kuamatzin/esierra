1ca2dec438dfcd9b991d2507259d32a328013c9a
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-02-06 10:16:56 -0600

    Redirect a /admin

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function __construct()
-    {
-        dd("HOLA");
-    }
-
     public function index()
     {
         return view('page.index');
